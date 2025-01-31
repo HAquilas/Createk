75cf685cce22d1a42acc70c65a7ba28e6caa8fe7
Naldohdh <Roy-ronald.hedihon-kassa@epitech.eu>
2025-01-31 20:30:06 +0100

    [add] end of the class Dotindicator

--- mobile/frip/lib/onboarding.dart
+++ mobile/frip/lib/onboarding.dart
@@ -92,3 +92,19 @@ class DotIndicator extends StatelessWidget {
     super.key,
     this.isActive = false
   });
+
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      height: isActive ? 12 : 4,
+      width: 4,
+      decoration: BoxDecoration(
+        color: isActive ? Colors.blue : const Color.fromARGB(139, 33, 149, 243),
+        borderRadius: BorderRadius.all(Radius.circular(12))
+      ),
+    );
+  }
+}
+
