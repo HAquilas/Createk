1e97a9821a6f71d243f0ee3e8b125778d1655f2c
Naldohdh <Roy-ronald.hedihon-kassa@epitech.eu>
2025-01-31 21:51:52 +0100

    [add] splash screen full made

--- mobile/frip/lib/splash.dart
+++ mobile/frip/lib/splash.dart
@@ -23,3 +23,56 @@ class _SplashScreenState extends State<SplashScreen> {
     super.dispose();
   }
 
+  Future<void> _isFirstTime() async {
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      bool isFirstTime = prefs.getBool("first_time") ?? true;
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      if (!_mounted) return;
+
+      if (isFirstTime) {
+        await prefs.setBool("first_time", false);
+        if (!_mounted) return;
+        Navigator.pushReplacementNamed(context, "/onboarding");
+      } else {
+        if (!_mounted) return;
+        Navigator.pushReplacementNamed(context, "/signin");
+      }
+    } catch (e) {
+      debugPrint("Error in splash screen: $e");
+      if (_mounted) {
+        Navigator.pushReplacementNamed(context, "/onboarding");
+      }
+    }
+  }
+
+  Future<bool> _checkAuthentication() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userToken') != null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 10, 101, 146),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo/Frip.png',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
