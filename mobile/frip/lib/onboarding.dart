68892ff76f1dd9c705b154199be03d1f4e801bfd
Naldohdh <Roy-ronald.hedihon-kassa@epitech.eu>
2025-01-31 20:27:13 +0100

    [add] create the onboard

---
+++ mobile/frip/lib/onboarding.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class OnboardingScreen extends StatefulWidget {
+  const OnboardingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
