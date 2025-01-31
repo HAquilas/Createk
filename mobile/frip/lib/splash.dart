132fb29ba61d96ebf5544136be555b77b9cb53e3
Naldohdh <Roy-ronald.hedihon-kassa@epitech.eu>
2025-01-31 21:50:29 +0100

    [add] creation of splash art

---
+++ mobile/frip/lib/splash.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
